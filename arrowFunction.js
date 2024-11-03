816ebd4e14ae72b1d43b59dd3de3353443e00b60
KwSalem <167813808+KwSalem@users.noreply.github.com>
2024-11-03 21:29:10 +0300

    Done all tasks

---
+++ arrowFunction.js
@@ -0,0 +1,20 @@
+// Task 1;
+
+// Rewrite the following function as an arrow function:
+/******************************
+	function greet(name) {
+	  return `Hello, ${name}!`;
+	}
+********************************/
+const greet = (name) => `Hello, ${name}!`;
+console.log(greet("Salem"), "-Task 1");
+//--------------------------------------------------------------------------
+//Task 2;
+// Write a simple arrow function that takes two parameters and returns their sum.
+const simple = (a, b) => a + b;
+console.log(simple(6, 5), "-Task 2");
+//---------------------------------------------------------------------------
+// Task 3;
+const squa = (b) => b * b;
+console.log(squa(6, 5), "-Task 3");
+//---------------------------------------------------------------------------
